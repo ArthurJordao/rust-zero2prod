4ee0421e498caa6c8b7015b87f6815f03ad5c9aa
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2023-05-14 12:31:25 -0300

    Add configuration for the app

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,36 @@
+use config::{Config, ConfigError, File};
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
 
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: Option<String>,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, ConfigError> {
+    let settings = Config::builder()
+        .add_source(File::with_name("configuration"))
+        .build()?;
+    settings.try_deserialize()
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        let password_string = self
+            .password
+            .clone()
+            .map(|password| format!(":{}", password))
+            .unwrap_or(String::from(""));
+        format!(
+            "postgres:://{}{}@{}:{}/{}",
+            self.username, password_string, self.host, self.port, self.database_name
+        )
+    }
+}

5ba091ac6b6f74a1a254117d3070ed4153bb4fa4
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2023-05-25 00:16:27 -0300

    Remove unneeded code/use as ref for subscriber name

--- src/domain.rs
+++ src/domain.rs
@@ -22,16 +22,10 @@ impl SubscriberName {
             Ok(Self(s))
         }
     }
+}
 
-    pub fn inner(self) -> String {
-        self.0
-    }
-
-    pub fn inner_mut(&mut self) -> &mut String {
-        &mut self.0
-    }
-
-    pub fn inner_ref(&self) -> &str {
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
         &self.0
     }
 }
